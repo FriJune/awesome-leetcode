3fbde0788a610e87faa5a569e3677fbb46243adc
ZhouXu <kirving0815@gmail.com>
2018-08-07 23:42:54 +0800

    feat: init project

---
+++ src/com/_test_/ListNode.java
@@ -0,0 +1,16 @@
+package com._test_;
+
+/**
+ * Definition for singly-linked list
+ *
+ * @author ZhouXu
+ * @version v1.0 2018-08-07
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
