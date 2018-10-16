92a61e6a53adb10af2acfab76c52de7ef951da0a
ZhouXu <kirving0815@gmail.com>
2018-10-16 10:40:26 +0800

    Implement Trie (Prefix Tree)

---
+++ src/com/leetcode/medium/_208/Trie.java
@@ -0,0 +1,92 @@
+package com.leetcode.medium._208;
+
+/**
+ * Implement a trie with insert, search, and startsWith methods.
+ *
+ * Example:
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // returns true
+ * trie.search("app");     // returns false
+ * trie.startsWith("app"); // returns true
+ * trie.insert("app");
+ * trie.search("app");     // returns true
+ *
+ * Note:
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * All inputs are guaranteed to be non-empty strings.
+ *
+ * @author ZhouXu
+ * @version V1.0 2018-10-16
+ */
+public class Trie {
+    class TrieNode {
+        private TrieNode[] children;
+        private boolean isWord;
+        public TrieNode() {
+            children = new TrieNode[26];
+            isWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        char[] chars = word.toCharArray();
+        TrieNode node = root;
+        for (int i = 0; i < chars.length; i++) {
+            int index = chars[i] - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isWord;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        char[] chars = prefix.toCharArray();
+        TrieNode node = root;
+        for (int i = 0; i < chars.length; i++) {
+            int index = chars[i] - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+
+    /** unit test */
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+        System.out.println(trie.search("apple")); // returns true
+        System.out.println(trie.search("app")); // returns false
+        System.out.println(trie.startsWith("app")); // returns true
+
+        trie.insert("app");
+        System.out.println(trie.search("app")); // returns true
+    }
+}
