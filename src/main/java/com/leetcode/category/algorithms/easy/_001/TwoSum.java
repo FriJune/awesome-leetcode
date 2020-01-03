596348d000249c55dd1823672a559c92fd2aed9e
zhoux <zhoux1@corp.21cn.com>
2020-01-03 11:00:37 +0800

    feat:Two Sum

--- src/main/java/com/leetcode/category/algorithms/easy/_001/TwoSum.java
+++ src/main/java/com/leetcode/category/algorithms/easy/_001/TwoSum.java
@@ -5,6 +5,7 @@ import com.leetcode.category.common.util.ToolUtil;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 /**
  * @author zhouxu
@@ -19,9 +20,29 @@ public class TwoSum {
     // Because nums[0] + nums[1] = 2 + 7 = 9,
     // return [0, 1].
     public int[] twoSum(int[] nums, int target) {
-        if (nums == null) {
-            return new int[0];
+        if (nums == null) return new int[0];
+
+        // for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+        //     if (nums[i] + nums[j] == target) {
+        //         return new int[]{i, j};
+        //     }
+        //     for (int k = i + 1; k < j; k++) {
+        //         if (nums[i] + nums[k] == target) {
+        //             return new int[]{i, k};
+        //         } else if (nums[k] + nums[j] == target) {
+        //             return new int[]{k, j};
+        //         }
+        //     }
+        // }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
         }
+
         return new int[0];
     }
 
